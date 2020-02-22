2510361101f0fcaafcf066a531d114fdbaa89ab1
LOGEN <1300141550@qq.com>
2020-02-22 23:32:42 +0800

    hello-word测试

---
+++ consumer_financial/hello_demo/src/main/java/com/financial/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.financial.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(){
+        return "hello 五金理财！";
+    }
+}
